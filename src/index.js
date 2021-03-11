d9dd51a6b619353da5fe25f3a5c8b209c384a21b
vanzo16 <vanzo16@ya.ru>
2021-03-11 11:07:44 +0300

    add reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function reverse (n) {
-  
+const reversed = num => parseFloat(num.toString().split('').reverse().join('')) * Math.sign(num)
+
+
+
+function reverse(num) {
+	return (
+    parseFloat(num.toString().split('').reverse().join('')) * Math.sign(num)
+  )
 }
+
+console.log(reversed(123))
+
+console.log(reversed(233))
+
+console.log(reversed(535))
+
+console.log(reversed(95034))
